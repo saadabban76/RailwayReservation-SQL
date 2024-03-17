c049beb4037915714695584d6e18f68b2b8c15db
saadabban76 <saadabban76@gmail.com>
2024-03-17 23:58:01 +0530

    checkpoint : frontend part done

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,66 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./db.js");
+const bcrypt = require("bcrypt"); // checks whether the password is valid or not
 
 const app = express();
-app.use(cors);
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200); 
-    res.send("Welcome to root URL of Server"); 
+  res.status(200);
+  res.send("Welcome to root URL of Server");
 });
 
-app.post("/signup", (req, res) => {
-  console.log("req body : ", req.body);
-  return req.body;
-  const query = `INSERT INTO users (f_name, l_name, email, password, gender, marital, dob, mobile) VALUES
-    ('mohammed', 'saad', 'saad@gmail.com', '123456789', 'male', 'unmarried', '2003-04-02', 9453890182);`;
+app.post("/signup", async (req, res) => {
+  const user = await req.body;
+  console.log("fName : ", user.fName);
+  const query = `INSERT INTO users (f_name, l_name, email, password, gender, marital, dob, mobile) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  db.query(
+    query,
+    [
+      user.fName,
+      user.lName,
+      user.email,
+      user.password,
+      user.gender,
+      user.marital_status,
+      user.dob,
+      user.mobile
+    ],
+    (err) => {
+      if (err) throw err;
+      console.log("res : ", res);
+      console.log(`1 record inserted`);
+      res.status(200).send({ message: "User created successfully." });
+    }
+  );
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = await req.body;
+  const query = "SELECT * FROM users WHERE f_name = ?";
+
+  db.query(query, [username], async (err, result) => {
+    if (err) throw err;
+
+    if ((await result.length) === 0) {
+      return res.status(401).send({ message: "Invalid email or password." });
+    }
+
+    const user = result[0]; // returns any 1 value that matches the result.
+    console.log("result : ", result);
+
+    const isPasswordValid = password === String(user.password);
+    // you can also use bcrypt for password comparison.
+
+    if (!isPasswordValid) {
+      return res.status(401).send({ message: "Invalid email or password." });
+    }
+
+    res.status(200).send({ message: "User logged in successfully" });
+  });
 });
 
 app.listen(3000, (err) => {
@@ -29,6 +75,6 @@ app.listen(3000, (err) => {
       }
     });
   } else {
-      console.log('error occured in server : ', err);
+    console.log("error occured in server : ", err);
   }
 });
